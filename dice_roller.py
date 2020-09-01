ae0e0d3831e667559c392323a1c43b862ce9a3b1
Gilles <gilles@Mini-de-Gilles.local>
2020-09-01 18:42:54 +0200

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,18 @@
 import random
 
 def main():
-  dice_rolls = 2
+  dice_rolls = int ( input ( 'Combien de dés voulez-vous lancer?' ))
+  dice_size  =  int ( input ( 'Combien de côtés ont les dés?' ))
   dice_sum = 0
   for  i in range(0, dice_rolls):
-    rouler = random.randint(1,6)
+    rouler = random.randint(1,dice_size)
     dice_sum += rouler
-    print("Vous avez lancer un ", rouler)
+    if rouler == 1 :
+      print("Vous avez obtenu in ", rouler, " ! critical fail")
+    elif rouler == dice_size:
+      print("Vous avez obtenu un ", rouler, " ! Critical success!")
+    else :
+      print("Vous avez lancer un ", rouler)
   print("Vous avez obtenu un total de ", dice_sum)
 
 if __name__== "__main__":
